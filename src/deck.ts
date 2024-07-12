2d3711f42801d663a14f21b7b5386c046266d7bc
ThePapaG <gdare1@yahoo.com.au>
2024-07-12 22:55:55 +1000

    Comment deck.ts

--- src/deck.ts
+++ src/deck.ts
@@ -1,8 +1,13 @@
 import { Card, CardDetails } from "./card.js";
 
+/** Represents a deck of cards */
 export class Deck {
     private _cards: Card[];
 
+    /**
+     * Creates a new Deck
+     * @param cards - Initial array of Cards
+     */
     constructor(cards: Card[]) {
         const missingCount = 40 - cards.length;
         if (missingCount > 0) {
@@ -12,17 +17,20 @@ export class Deck {
         this.shuffle();
     }
 
+    /** Creates a deep copy of the deck */
     deepCopy(): Deck {
         const newDeck = new Deck([]);
         newDeck._cards = this._cards.map(card => new Card(card.name, { ...card.details }));
         return newDeck;
     }
 
+    /** Draws a random card from the deck */
     drawCard(): Card {
         const index = Math.floor(Math.random() * this._cards.length);
         return this._cards.splice(index, 1)[0];
     }
 
+    /** Shuffles the deck */
     shuffle(): void {
         for (let i = this._cards.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -30,15 +38,22 @@ export class Deck {
         }
     }
 
-    get deckList(): Card[] {
-        return this._cards;
+    /** Gets the list of cards in the deck */
+    get deckList(): Readonly<Card[]> {
+        return Object.freeze(this._cards);
     }
 
+    /** Gets the number of cards in the deck */
     get deckCount(): number {
         return this._cards.length;
     }
 }
 
+/**
+ * Builds a deck from a record of card details
+ * @param deckList - Record of card names and their details
+ * @returns A new Deck instance
+ */
 export function buildDeck(deckList: Record<string, CardDetails>): Deck {
     const cards: Card[] = [];
     for (const [card, details] of Object.entries(deckList)) {
