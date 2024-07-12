3f14594ccaf255a1394f0e8623537eb0b5761c47
ThePapaG <gdare1@yahoo.com.au>
2024-07-12 23:04:42 +1000

    C omment game state

--- src/game-state.ts
+++ src/game-state.ts
@@ -1,21 +1,25 @@
 import { Card } from "./card";
 import { Deck } from "./deck";
 
+/** Represents the current state of a game */
 export class GameState {
     private _deck: Deck;
     private _hand: Card[];
-    private _banishPile: Card[];
-    private _graveyard: Card[];
+    private _banishPile: Card[] = [];
+    private _graveyard: Card[] = [];
 
+    /**
+     * Creates a new GameState
+     * @param deck - The deck to use for this game
+     * @param handSize - The number of cards to draw for the initial hand
+     */
     constructor(deck: Deck, handSize: number = 5) {
         this._deck = deck.deepCopy();
         this._deck.shuffle();
         this._hand = [...Array(handSize)].map(() => this._deck.drawCard());
-
-        this._banishPile = [];
-        this._graveyard = [];
     }
 
+    /** Creates a deep copy of the game state */
     deepCopy(): GameState {
         const newState = new GameState(this._deck);
         this._deck = this._deck.deepCopy(); // need to recopy the deck since the constructor will draw a hand
@@ -25,19 +29,23 @@ export class GameState {
         return newState;
     }
 
-    get deck(): Deck {
+    /** Gets the current deck */
+    get deck(): Readonly<Deck> {
         return this._deck;
     }
 
+    /** Gets the current hand */
     get hand(): Card[] {
         return this._hand;
     }
 
-    get banishPile(): Card[] {
-        return this._banishPile;
+    /** Gets the banish pile */
+    get banishPile(): Readonly<Card[]> {
+        return Object.freeze(this._banishPile);
     }
 
-    get graveyard(): Card[] {
-        return this._graveyard;
+    /** Gets the graveyard */
+    get graveyard(): Readonly<Card[]> {
+        return Object.freeze(this._graveyard);
     }
 }
