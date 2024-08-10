3c9124366b33bd87d61f39418e0b2024ac0ea46d
ThePapaG <gdare1@yahoo.com.au>
2024-08-10 22:50:18 +1000

    fix lint

--- src/components/CardTable.tsx
+++ src/components/CardTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import {
     Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper,
     Checkbox, TextField, IconButton, TablePagination, Toolbar, Typography,
