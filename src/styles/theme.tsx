caff2f62ab07602def6a104c10efc659f260b562
ThePapaG <gdare1@yahoo.com.au>
2024-09-07 09:02:28 +1000

    Add more colouring to the theme

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -2,12 +2,29 @@ import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
     palette: {
+        mode: 'dark',
         primary: {
             main: '#018F85',
-            contrastText: 'white',
         },
         secondary: {
             main: '#C00086',
+        },
+        background: {
+            default: '#18191D',
+            paper: '#333436',
+        },
+        text: {
+            primary: '#ffffff',
+            secondary: '#ffffff',
+        },
+    },
+    components: {
+        MuiTypography: {
+            styleOverrides: {
+                root: {
+                    color: '#ffffff',
+                },
+            },
         },
     },
 });
