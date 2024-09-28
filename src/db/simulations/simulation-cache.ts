e29e557dffb7c2f9c5228e57a1ee8cde5271b474
ThePapaG <gdare1@yahoo.com.au>
2024-09-28 22:41:55 +1000

    Fix if the cache is stale to not fill browser cache

--- src/db/simulations/simulation-cache.ts
+++ src/db/simulations/simulation-cache.ts
@@ -2,9 +2,16 @@ import { CardDetails } from "../../core/data/card-details";
 import { SimulationInput } from "../../core/data/simulation-input";
 import { getSimulationData, SimulationData } from "./get";
 
+interface CacheEntry {
+    data: SimulationData;
+    timestamp: number;
+}
+
+const STALE_DATE = new Date().setDate(new Date().getDate() - 30);
+
 class SimulationCache {
     private static instance: SimulationCache;
-    private cache: Map<string, SimulationData> = new Map();
+    private cache: Map<string, CacheEntry> = new Map();
     private readonly maxCacheSize: number = 100;
 
     private constructor() {
@@ -18,18 +25,44 @@ class SimulationCache {
         return SimulationCache.instance;
     }
 
+    private cleanupCache(): void {
+        // Cleanup stale entries
+        {
+            const keys = Array.from(this.cache.keys());
+            for (const key of keys) {
+                const entry = this.cache.get(key);
+                if (entry && entry.timestamp < STALE_DATE) {
+                    this.cache.delete(key);
+                }
+            }
+        }
+
+        // Cleanup size
+        if (this.cache.size > this.maxCacheSize) {
+            const keys = Array.from(this.cache.keys());
+            for (let i = 0; i < keys.length - this.maxCacheSize; i++) {
+                this.cache.delete(keys[i]);
+            }
+        }
+
+        this.save();
+    }
+
     public getCachedSimulations(): SimulationData[] {
-        return Array.from(this.cache).map(([, data]) => data);
+        return Array.from(this.cache).map(([, entry]) => entry.data);
     }
 
     public async getSimulationById(id: string): Promise<SimulationData | undefined> {
         if (!this.cache.has(id)) {
             const storedData = await getSimulationData(id);
-            this.cache.set(id, storedData);
-            this.save();
+            this.cache.set(id, {
+                data: storedData,
+                timestamp: Date.now()
+            });
+            this.cleanupCache();
         }
 
-        return this.cache.get(id);
+        return this.cache.get(id)?.data;
     }
 
     public async getSimulationInputById(id: string): Promise<SimulationInput | undefined> {
@@ -48,11 +81,6 @@ class SimulationCache {
             deck,
             conditions: json.conditions
         };
-    }
-
-    public clear(): void {
-        this.cache = new Map();
-        this.save();
     }
 
     private load(): void {
